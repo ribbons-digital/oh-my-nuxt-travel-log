72c70f82cdd6f79bed2f8a5b918dff124a67698a
Ryan LIU <ryan@ribbons.digital>
2025-04-30 16:22:36 +1000

    add landing page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Record your travel experiences and share them with the world.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
