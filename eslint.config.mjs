6b3a6a89f74769c28dc7969d597a9d0ecc2099fe
Ryan <develop@ribbons.digital>
2025-04-30 14:20:22 +1000

    setup tailwind and daisyui (#49)
    Co-authored-by: Ryan LIU <ryan@ribbons.digital>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,10 +15,7 @@ export default withNuxt(
         semi: true,
         quotes: "double",
       },
-<<<<<<< HEAD
       ignores: [".pnpm-store/**"],
-=======
->>>>>>> 0ceb869 (add linting configs)
     },
     {
       rules: {
