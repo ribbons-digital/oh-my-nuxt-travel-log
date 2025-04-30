587f5930ab7ba90f9fbc411c4b8533d1db78790e
Ryan <develop@ribbons.digital>
2025-04-30 16:25:06 +1000

    Omntl 11 (#50)
    * setup tailwind and daisyui
    
    * add landing page
    
    ---------
    
    Co-authored-by: Ryan LIU <ryan@ribbons.digital>

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink class="btn btn-ghost text-xl" to="/">
+        Nuts travel log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a href="#" class="btn btn-accent">Sign In with Github <Icon name="tabler:brand-github" size="24" /></a>
+    </div>
+  </div>
+</template>
