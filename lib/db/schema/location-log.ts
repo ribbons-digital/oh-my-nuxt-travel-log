ec3d7993b5034b4514bd995808cca224b7da7e52
Ryan <develop@ribbons.digital>
2025-05-01 10:42:47 +1000

    Omntl 14 (#53)
    * add db schemas
    
    * update gh action yaml to include envs
    
    ---------
    
    Co-authored-by: Ryan LIU <ryan@ribbons.digital>

---
+++ lib/db/schema/location-log.ts
@@ -0,0 +1,16 @@
+import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+import { location } from "./location";
+
+export const locationLog = sqliteTable("locationLog", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  description: text(),
+  lat: real().notNull(),
+  long: real().notNull(),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+  startedAt: int().notNull(),
+  endedAt: int().notNull(),
+  locationId: int().notNull().references(() => location.id),
+});
