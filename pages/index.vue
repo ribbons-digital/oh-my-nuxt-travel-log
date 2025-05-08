6269d3d5dbac9c6edf611a5171499961c05373ff
Ryan LIU <ryan@ribbons.digital>
2025-05-09 08:48:04 +1000

    add server middleware for auth

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -8,7 +12,15 @@
         <p class="py-6">
           Record your travel experiences and share them with the world.
         </p>
-        <AuthButton />
+        <AuthButton v-if="!authStore.user" />
+
+        <NuxtLink
+          v-else
+          to="/dashboard"
+          class="btn btn-primary"
+        >
+          Start log  in
+        </NuxtLink>
       </div>
     </div>
   </div>
