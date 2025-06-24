70285d3c6fb3696a84516c0cdcb53b240afa3372
Ryan LIU <ryan@ribbons.digital>
2025-06-24 14:33:36 +1000

    complete #24

--- server/api/location.post.ts
+++ server/api/location.post.ts
@@ -1,5 +1,13 @@
+import type { DrizzleError } from "drizzle-orm";
+
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import slugify from "slug";
+
 import db from "~/lib/db";
 import { InsertLocation, location } from "~/lib/db/schema";
+
+const nanoid = customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 5);
 
 export default defineEventHandler(async (event) => {
   if (!event.context.user) {
@@ -29,11 +37,52 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
-  const [createdLocation] = await db.insert(location).values({
-    ...result.data,
-    userId: event.context.user.id,
-    slug: result.data.name.toLowerCase().replace(/ /g, "-"),
-  }).returning();
+  const existingLocation = await db.query.location.findFirst({
+    where: and(eq(location.name, result.data.name), eq(location.userId, event.context.user.id)),
+  });
+  if (existingLocation) {
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: "Location with this name already exists",
+    }));
+  }
 
-  return createdLocation;
+  let slug = slugify(result.data.name);
+  let existing = !!(await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  }));
+
+  while (existing) {
+    const id = nanoid();
+    const newSlug = `${slug}-${id}`;
+    slug = newSlug;
+    existing = !!(await db.query.location.findFirst({
+      where: eq(location.slug, slug),
+    }));
+
+    if (!existing) {
+      slug = newSlug;
+    }
+  }
+
+  try {
+    const [createdLocation] = await db.insert(location).values({
+      ...result.data,
+      userId: event.context.user.id,
+      slug,
+    }).returning();
+    return createdLocation;
+  }
+  catch (error) {
+    const e = error as DrizzleError;
+
+    if (e.message === "SQLITE_CONSTRAINT: SQLite error: UNIQUE constraint failed: location.name, location.user_id") {
+      return sendError(event, createError({
+        statusCode: 409,
+        statusMessage: "Location with this name already exists",
+      }));
+    }
+
+    throw error;
+  }
 });
