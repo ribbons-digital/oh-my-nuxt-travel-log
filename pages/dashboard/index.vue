7cc530b70b0944ddc85e007f8e3081a21d7093ca
Ryan <develop@ribbons.digital>
2025-06-28 13:50:11 +1000

    complete #24 (#62)
    Co-authored-by: Ryan LIU <ryan@ribbons.digital>

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const { data: locations, status } = await useFetch("/api/locations", {
+  lazy: true,
+});
+</script>
+
+<template>
+  <div class="p-4">
+    <h2 class="text-2xl">
+      Locations
+    </h2>
+    <div v-if="status === 'pending'">
+      <span class="loading loading-spinner loading-xl" />
+    </div>
+    <div v-else-if="locations && locations.length > 0" class="flex flex-wrap gap-4 mt-4">
+      <div
+        v-for="location in locations"
+        :key="location.id"
+        class="card card-compact bg-base-300 h-40 w-72"
+      >
+        <div class="card-body">
+          <h3 class="text-xl">
+            {{ location.name }}
+          </h3>
+          <p>
+            {{ location.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+    <div v-else class="flex flex-col gap-2 mt-4">
+      <p>
+        Add a new location to get started.
+      </p>
+      <NuxtLink to="/dashboard/add" class="btn btn-primary w-40">
+        Add Location
+        <Icon
+          name="tabler:circle-plus-filled"
+          size="24"
+        />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
